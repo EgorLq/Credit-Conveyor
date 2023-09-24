6b27173fba69144eb4e05b9023b9762002079bb5
Egor <52930332+EgorLq@users.noreply.github.com>
2023-09-24 11:49:11 +0300

    update project

--- dossier/src/main/java/com/neoflex/java/DossierApplication.java
+++ dossier/src/main/java/com/neoflex/java/DossierApplication.java
@@ -10,5 +10,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class DossierApplication {
     public static void main(String[] args) {
         ApplicationContext context = new SpringApplicationBuilder(DossierApplication.class).headless(false).run(args);
+
     }
 }
